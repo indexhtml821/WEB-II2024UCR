54f0e3d631491ad37d9bc36a9dafa3530fa1934f
indexhtml821 <isaacopine@gmail.com>
2024-10-24 23:33:19 -0600

    updates to check available ports

--- My Web Sites/Visitas primitivo en node.js/visitas.js
+++ My Web Sites/Visitas primitivo en node.js/visitas.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const url = require('url');
 const querystring = require('querystring');
 
-const hostname = '127.0.0.1';
+const hostname = '0.0.0.1';
 const port = 3000;
 
 const visitasFile = 'visitas.txt';
