ae05c4ce8ce193900476ecbded5abd699580ec9d
indexhtml821 <isaacopine@gmail.com>
2024-10-25 00:34:12 -0600

    elimina ñ en el codigo

--- My Web Sites/Visitas primitivo en node.js/visitas.js
+++ My Web Sites/Visitas primitivo en node.js/visitas.js
@@ -76,7 +76,7 @@ const server = http.createServer((req, res) => {
                 // Convert the file content to HTML
                 const entries = data.toString().split('\n\n');
                 let htmlContent = '<html><body>';
-                htmlContent += '<br><a href="/">Añadir otra visita</a><br><br>';
+                htmlContent += '<br><a href="/">Enviar otra visita</a><br><br>';
                 entries.forEach(entry => {
                     if (entry.trim()) {
                         const lines = entry.split('\n');
