a5035fae33fa05651ef555ebf8fea425c49fbd82
indexhtml821 <isaacopine@gmail.com>
2024-10-26 18:03:16 -0600

    updates controller.js

--- directionsdatabase/blog/blog/routes/controllers.js
+++ directionsdatabase/blog/blog/routes/controllers.js
@@ -1,18 +1,64 @@
 const express = require('express');
 const router = express.Router();
-const controllers = require('require.all')('../controllers');
+const controllers = require('require.all')('../controllers'); // Asegúrate de que esta ruta sea correcta
 
-router
-.all('/:controller', (req, res, next) => {
+// Middleware para manejar las solicitudes sin un controlador válido
+const handleControllerNotFound = (req, res) => {
+    res.status(404).send('Controller not found');
+};
 
-    (new controllers[req.params.controller]).index(req, res, next);
-})
-.all('/:controller/:action', (req, res, next) => {
-    (new controllers[req.params.controller])[req.params.action](req, res, next);
-})
-.all('/:controller/:action/:id', (req, res, next) => {
-    (new controllers[req.params.controller])[req.params.action](req, res, next);
+// Ruta para manejar solicitudes a un controlador específico
+router.all('/:controller', (req, res, next) => {
+    const ControllerClass = controllers[req.params.controller];
+
+    // Verifica si el controlador existe
+    if (ControllerClass) {
+        const controllerInstance = new ControllerClass();
+        // Verifica si el método 'index' existe
+        if (typeof controllerInstance.index === 'function') {
+            return controllerInstance.index(req, res, next);
+        } else {
+            return res.status(404).send('Method index not found');
+        }
+    } else {
+        return handleControllerNotFound(req, res);
+    }
 });
-   
+
+// Ruta para manejar solicitudes a un controlador y una acción específica
+router.all('/:controller/:action', (req, res, next) => {
+    const ControllerClass = controllers[req.params.controller];
+
+    // Verifica si el controlador existe
+    if (ControllerClass) {
+        const controllerInstance = new ControllerClass();
+        // Verifica si la acción existe
+        if (typeof controllerInstance[req.params.action] === 'function') {
+            return controllerInstance[req.params.action](req, res, next);
+        } else {
+            return res.status(404).send('Method not found');
+        }
+    } else {
+        return handleControllerNotFound(req, res);
+    }
+});
+
+// Ruta para manejar solicitudes a un controlador, acción y un ID específico
+router.all('/:controller/:action/:id', (req, res, next) => {
+    const ControllerClass = controllers[req.params.controller];
+
+    // Verifica si el controlador existe
+    if (ControllerClass) {
+        const controllerInstance = new ControllerClass();
+        // Verifica si la acción existe
+        if (typeof controllerInstance[req.params.action] === 'function') {
+            return controllerInstance[req.params.action](req, res, next);
+        } else {
+            return res.status(404).send('Method not found');
+        }
+    } else {
+        return handleControllerNotFound(req, res);
+    }
+});
 
 module.exports = router;
